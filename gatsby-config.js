ad2ca51379d6ae5126e7756fe963c15dc099a49c
Alex <sanya_renin@mail.ru>
2020-02-18 17:50:39 +0300

    added SCSS style

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,11 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Andrew Mead'
+  },
+  plugins: [
+    'gatsby-plugin-sass'
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import React from "react"
 
+import '../style/index.scss'
+
 export default () => <div>Hello world!</div>
