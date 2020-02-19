b12b72a501003a0f75dd0ed06211c4390b9ee98e
Alex <sanya_renin@mail.ru>
2020-02-19 11:42:12 +0300

    using gatsby-transformer-remark

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>Blog</h1>
-            <p>Posts will show up here later on.</p>
+            <ol>
+                {data.allMarkdownRemark.edges.map((edge, index) => {
+                  const { title, date } = edge.node.frontmatter
+
+                  return (
+                        <li
+                          key={title + index}
+                        >
+                            <h2>{title}</h2>
+                            <p>{date}</p>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -3,4 +3,10 @@ title: "The Great Gatsby Bootcamp"
 date: "2020-02-19"
 ---
 
-This is the post about Gatsby
+I just launched a new bootcamp!
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQL
+3. React
