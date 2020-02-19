4a1cf32dffbb97985a1e942f468eda94932e51b4
Alex <sanya_renin@mail.ru>
2020-02-19 16:09:47 +0300

    Dynamic Pages from Contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,43 +1,37 @@
-import React from 'react'
+import React from "react"
+import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-import { graphql } from 'gatsby'
-
-import Layout from '../components/layout'
+import Layout from "../components/layout"
 
 export const query = graphql`
-  query (
-    $slug: String!
-  ) {
-    markdownRemark(
-      fields: {
-        slug: {
-          eq: $slug
-        }
-        
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
       }
-    ) {
-      frontmatter {
-        title
-        date
-      }
-      html
     }
   }
 `
 
-const Blog = (props) => {
-  const { title, date } = props.data.markdownRemark.frontmatter
+const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = node.data.target.fields.title['en-US']
+        const url = node.data.target.fields.file['en-US'].url
+        return <img alt={alt} src={url} />
+      }
+    }
+  }
+
   return (
     <Layout>
-      <h1>{title}</h1>
-      <p>{date}</p>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: props.data.markdownRemark.html
-        }}
-      >
-
-      </div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
     </Layout>
   )
 }
